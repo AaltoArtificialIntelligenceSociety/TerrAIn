e7524d9d3d72546ae06c4956210cf2c80c53e42c
Perttu Yli-Opas <perttuyo@gmail.com>
2018-05-03 23:16:21 +0300

    Fixed all the bugs. Except the new ones

--- src/js/main.js
+++ src/js/main.js
@@ -5,6 +5,9 @@ var concepts = [];
 concepts[0] = {"text":"machine learning", "size":50, "pos":[[100, 500], [300, 300]]};
 
 const canvas = document.getElementById("goCanvas");
+canvas.setAttribute('width', w);
+canvas.setAttribute('height', h);
+
 const g = canvas.getContext("2d");
 
 var state = 0;
@@ -14,24 +17,27 @@ update();
 
 function update(){
 	//BG
-	g.fillStyle="#9e662e";
+	g.fillStyle="#BBB";
 	g.fillRect(0, 0, w, h);
 	
-	g.beginPath();
-	size = concepts[0].size;
-	x = concepts[0].pos[state][0];
-	y = concepts[0].pos[state][1];
-	g.arc(x, y, size/2, 0, 2 * Math.PI);
+	concepts.forEach(function(c){
 	
-	g.fillStyle = 'blue';
-	
-	g.fill();
-	g.lineWidth = 0.5;
-	g.strokeStyle = 'black';
-	g.stroke();
+		g.beginPath();
+		g.arc(c.pos[state][0], c.pos[state][1], c.size/2, 0, 2 * Math.PI);
+		
+		g.fillStyle = 'royalBlue';
+		
+		g.fill();
+		g.lineWidth = 0.5;
+		g.strokeStyle = 'black';
+		g.stroke();
+	})
 }
 
 function move(){
 	state = 1 - state;
 	update();
+}
+function mouseMoved(){
+	
 }
