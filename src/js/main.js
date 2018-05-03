c0cf8521e35a76ca682342cba0cc045c33c3081a
Perttu Yli-Opas <perttuyo@gmail.com>
2018-05-03 23:25:15 +0300

    Animation!

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@ const w = 1000, h = 600;
 var concepts = [];
 
 concepts[0] = {"text":"machine learning", "size":50, "pos":[[100, 500], [300, 300]]};
+concepts[1] = {"text":"deep learning", "size":50, "pos":[[200, 300], [700, 200]]};
 
 const canvas = document.getElementById("goCanvas");
 canvas.setAttribute('width', w);
@@ -12,10 +13,15 @@ const g = canvas.getContext("2d");
 
 var state = 0;
 
+const animSteps = 11;
+var anim = [0, 0.05, 0.11, 0.18, 0.3, 0.5, 0.7, 0.82, 0.89, 0.95, 1];
 
 update();
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
-function update(){
+function update(step){
 	//BG
 	g.fillStyle="#BBB";
 	g.fillRect(0, 0, w, h);
@@ -23,7 +29,9 @@ function update(){
 	concepts.forEach(function(c){
 	
 		g.beginPath();
-		g.arc(c.pos[state][0], c.pos[state][1], c.size/2, 0, 2 * Math.PI);
+		var x = step * c.pos[state][0] + (1-step) * c.pos[1-state][0];
+		var y = step * c.pos[state][1] + (1-step) * c.pos[1-state][1];
+		g.arc(x, y, c.size/2, 0, 2 * Math.PI);
 		
 		g.fillStyle = 'royalBlue';
 		
@@ -34,9 +42,12 @@ function update(){
 	})
 }
 
-function move(){
+async function move(){
 	state = 1 - state;
-	update();
+	for(var i = 0; i < animSteps; i++){
+		update(anim[i]);
+		await sleep(200);
+	}
 }
 function mouseMoved(){
 	
